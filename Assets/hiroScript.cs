d0014b01fe44a34064d6c1cc0ea3f4cc6cbb6812
VickaSvetlova <vickasvetlova@gmail.com>
2016-10-13 18:03:11 +0300

    стабил

--- Assets/hiroScript.cs
+++ Assets/hiroScript.cs
@@ -9,16 +9,19 @@ public class hiroScript : MonoBehaviour {
     public float strafe;
     public Collider graund;
     public bool jupp;
+    public GameObject hiroMesh;
+    public GameObject respPoint;
 
     // Use this for initialization
     void Start() {
         rb = GetComponent<Rigidbody>();
-
+        Invoke("PauseToRun", 1.25f);
+        jupp = true;
         }
 
     // Update is called once per frame
     void Update() {
-        if (!IsInvoking("StopMove")&&!jupp) {
+        if (!IsInvoking("StopMove") && !jupp && run) {
             if (Input.GetKeyDown(KeyCode.LeftArrow)) {
                 Left();
                 }
@@ -42,13 +45,13 @@ public class hiroScript : MonoBehaviour {
             }
         }
     void Left() {
-      
-        rb.velocity = new Vector3(maxSpeed, 0, strafe);
-        Invoke("StopMove",0.3f);
+
+        rb.velocity = new Vector3(maxSpeed, transform.position.y, strafe);
+        Invoke("StopMove", 0.3f);
         }
 
     void Right() {
-        rb.velocity = new Vector3(maxSpeed, 0, strafe*-1);
+        rb.velocity = new Vector3(maxSpeed, transform.position.y, strafe * -1);
         Invoke("StopMove", 0.3f);
         }
     void Jump() {
@@ -56,7 +59,7 @@ public class hiroScript : MonoBehaviour {
             rb.AddForce(Vector3.up * 300);
             jupp = true;
             }
-        
+
         }
 
     void StopMove() {
@@ -66,6 +69,31 @@ public class hiroScript : MonoBehaviour {
     void OnTriggerEnter(Collider coll) {
         if (coll.gameObject.CompareTag("ground")) {
             jupp = false;
+
             }
+        }
+    void OnTriggerExit(Collider coll) {
+        if (coll.gameObject.CompareTag("ground")) {
+            jupp = true;
+            }
+        }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("gate")) {
+            run = false;
+            hiroMesh.active = false;
+            Invoke("respPointS", 0.5f);
+            }
+
+        }
+
+    void PauseToRun() {
+        run = true;
+        }
+
+    void respPointS() {
+        hiroMesh.active = true;
+        transform.position = new Vector3(respPoint.transform.position.x, respPoint.transform.position.y, respPoint.transform.position.z);
+        Start();
         }
     }
